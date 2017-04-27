6467c7475f10298ee09dc0febebf55fafc221159
Nick Gill <nick@rocket55.com>
2017-04-27 15:22:26 -0500

    updated code

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,24 @@
 $(function() {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/Rocket55.json',
+        dataType: 'jsonp',
+        success: function(response) {
+
+            addCourses('response', response.courses.completed);
+        }
+    });
+    function addCourses(courses) {
+
+        var $badges = $('#badges');
+
+        courses.forEach( function(course){
+
+           $('<div />', {
+               'class': 'course'
+           }).appendTo($badges);
+
+        });
+    }
 
 });
